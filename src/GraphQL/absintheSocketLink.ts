13b0f5faa37d0919822beba58dd3df37d4dd4045
Franzé Jr <franzejr@gmail.com>
2019-10-21 19:24:48 -0300

    Update env file and add websocket url from env var

--- src/GraphQL/absintheSocketLink.ts
+++ src/GraphQL/absintheSocketLink.ts
@@ -1,7 +1,8 @@
 import * as AbsintheSocket from '@absinthe/socket'
 import { createAbsintheSocketLink } from '@absinthe/socket-apollo-link'
 import { Socket as PhoenixSocket } from 'phoenix'
+import Config from 'react-native-config'
 
 export default createAbsintheSocketLink(AbsintheSocket.create(
-    new PhoenixSocket('ws://localhost:4000/socket')
+    new PhoenixSocket(Config.WEBSOCKET_API_URL)
 ))
